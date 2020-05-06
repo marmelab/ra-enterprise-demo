843d7d62a7b317a7b775e536a32831605a715c65
Florian FERBACH <florian@marmelab.com>
2020-05-06 17:48:59 +0200

    Initial commit

---
+++ src/categories/index.ts
@@ -0,0 +1,10 @@
+import CategoryIcon from '@material-ui/icons/Bookmark';
+
+import CategoryList from './CategoryList';
+import CategoryEdit from './CategoryEdit';
+
+export default {
+    list: CategoryList,
+    edit: CategoryEdit,
+    icon: CategoryIcon,
+};
---
+++ src/reviews/index.js
@@ -0,0 +1,7 @@
+import ReviewIcon from '@material-ui/icons/Comment';
+import ReviewList from './ReviewList';
+
+export default {
+    icon: ReviewIcon,
+    list: ReviewList,
+};
