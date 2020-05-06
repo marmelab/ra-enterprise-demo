5928eaa761d2cddc92c26be07a5c828dd86a8a5c
Florian FERBACH <florian@marmelab.com>
2020-05-06 18:18:19 +0200

    Implement data for tours

---
+++ src/tours/data.js
@@ -0,0 +1,24 @@
+export default {
+  tours: [
+    {
+      id: 1,
+      image: "images/tours/ra-markdown.jpg",
+      title: "ra-markdown",
+      tour: "ra-markdown",
+      comment: `
+For applications that need to display and edit Markdown content.
+      `,
+      href: "http://marmelab.com/react-admin",
+    },
+    {
+      id: 2,
+      image: "images/tours/ra-preferences.jpg",
+      title: "ra-preferences",
+      tour: "ra-preferences",
+      comment: `
+This module provides a way to store and persist user preferences in local storage. These preferences are device dependent, so this module is particularly fitted for UI preferences.
+      `,
+      href: "http://marmelab.com/react-admin",
+    },
+  ],
+};
