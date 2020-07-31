46d43f2bc9c9899e67b5769930256ef4215c2f12
Luwangel <adrien.amoros@gmail.com>
2020-07-31 15:12:57 +0200

    Prettier format

--- src/products/Preview.js
+++ src/products/Preview.js
@@ -1,93 +1,93 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { MarkdownField } from "@react-admin/ra-markdown";
+import React from 'react';
+import { MarkdownField } from '@react-admin/ra-markdown';
+import { makeStyles } from '@material-ui/core/styles';
 
 const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    padding: "0 20px",
-    width: 350,
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '0 20px',
+        width: 350,
 
-    "& > p": {
-      fontSize: 12,
-      color: "#777",
-      textAlign: "right",
+        '& > p': {
+            fontSize: 12,
+            color: '#777',
+            textAlign: 'right',
+        },
     },
-  },
-  description: {
-    padding: "0 20px",
-    "& h1": {
-      fontSize: 18,
-      color: "#333",
-      fontWeight: "normal",
+    description: {
+        padding: '0 20px',
+        '& h1': {
+            fontSize: 18,
+            color: '#333',
+            fontWeight: 'normal',
+        },
+        '& h2': {
+            fontSize: 14,
+            color: '#666',
+            fontWeight: 'normal',
+        },
     },
-    "& h2": {
-      fontSize: 14,
-      color: "#666",
-      fontWeight: "normal",
+    preview: {
+        padding: '0 40px',
     },
-  },
-  preview: {
-    padding: "0 40px",
-  },
-  frame: {
-    position: "relative",
-    width: "100%",
-    paddingBottom: "82.5%",
-    background: "black",
-    boxShadow: "0 10px 7px -5px rgba(0, 0, 0, 0.3)",
-  },
-  mat: {
-    position: "absolute",
-    background: "white",
-    top: "3.0303%",
-    bottom: "3.0303%",
-    left: "2.5%",
-    right: "2.5%",
-    boxShadow: "0px 0px 20px 0px rgba(0,0,0,0.5) inset",
-  },
-  art: {
-    position: "absolute",
-    top: "16.129%",
-    bottom: "16.129%",
-    left: "13.158%",
-    right: "13.158%",
+    frame: {
+        position: 'relative',
+        width: '100%',
+        paddingBottom: '82.5%',
+        background: 'black',
+        boxShadow: '0 10px 7px -5px rgba(0, 0, 0, 0.3)',
+    },
+    mat: {
+        position: 'absolute',
+        background: 'white',
+        top: '3.0303%',
+        bottom: '3.0303%',
+        left: '2.5%',
+        right: '2.5%',
+        boxShadow: '0px 0px 20px 0px rgba(0,0,0,0.5) inset',
+    },
+    art: {
+        position: 'absolute',
+        top: '16.129%',
+        bottom: '16.129%',
+        left: '13.158%',
+        right: '13.158%',
 
-    "& img": {
-      width: "100%",
+        '& img': {
+            width: '100%',
+        },
     },
-  },
 };
 
 const useStyles = makeStyles(styles);
 
 const Preview = ({ record }) => {
-  const { image, height, width, price, reference, stock } = record;
-  const classes = useStyles();
+    const { image, height, width, price, reference, stock } = record;
+    const classes = useStyles();
 
-  return (
-    <div className={classes.container}>
-      <p>Preview</p>
-      <div className={classes.preview}>
-        <div className={classes.frame}>
-          <div className={classes.mat}>
-            <div className={classes.art}>
-              <img src={image} alt="" />
+    return (
+        <div className={classes.container}>
+            <p>Preview</p>
+            <div className={classes.preview}>
+                <div className={classes.frame}>
+                    <div className={classes.mat}>
+                        <div className={classes.art}>
+                            <img src={image} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div className={classes.description}>
+                <h1>Poster #${reference}</h1>
+                <h2>
+                    {width}x{height}
+                </h2>
+                <h2>{price} €</h2>
+                <MarkdownField record={record} source="description" />
+            </div>
         </div>
-      </div>
-      <div className={classes.description}>
-        <h1>Poster #${reference}</h1>
-        <h2>
-          {width}x{height}
-        </h2>
-        <h2>{price} €</h2>
-        <MarkdownField record={record} source="description" />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Preview;
--- src/tours/Tour.js
+++ src/tours/Tour.js
@@ -1,130 +1,134 @@
-import React from "react";
-import { useMutation, useRefresh } from "react-admin";
-import classnames from "classnames";
-
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import PlayIcon from "@material-ui/icons/PlayArrow";
-import { MarkdownField } from "@react-admin/ra-markdown";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import NewReleasesIcon from "@material-ui/icons/NewReleases";
-
-import { useTour } from "@react-admin/ra-tour";
+import React from 'react';
+import { useMutation, useRefresh } from 'react-admin';
+import { MarkdownField } from '@react-admin/ra-markdown';
+import { useTour } from '@react-admin/ra-tour';
+import classnames from 'classnames';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import PlayIcon from '@material-ui/icons/PlayArrow';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import NewReleasesIcon from '@material-ui/icons/NewReleases';
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    cursor: "pointer",
-    margin: 20,
-    opacity: 0.9,
-    transition: "opacity 0.3s ease-in-out",
-    "&:hover,&:focus-within": {
-      opacity: 1,
+    root: {
+        maxWidth: 345,
+        cursor: 'pointer',
+        margin: 20,
+        opacity: 0.9,
+        transition: 'opacity 0.3s ease-in-out',
+        '&:hover,&:focus-within': {
+            opacity: 1,
+        },
     },
-  },
-  visited: {
-    opacity: 0.6,
-  },
-  media: {
-    height: 140,
-  },
-  rightButton: {
-    marginLeft: "auto",
-  },
-  content: {
-    position: "relative",
-  },
-  newIcon: {
-    position: "absolute",
-    top: 10,
-    right: 10,
-  },
-  actions: {
-    paddingBottom: 0,
-  },
-  buyIcon: {
-    marginLeft: "auto !important",
-  },
-  playedOnIndicator: {
-    paddingLeft: 16,
-    paddingTop: 0,
-    paddingBottom: 16,
-  },
+    visited: {
+        opacity: 0.6,
+    },
+    media: {
+        height: 140,
+    },
+    rightButton: {
+        marginLeft: 'auto',
+    },
+    content: {
+        position: 'relative',
+    },
+    newIcon: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+    },
+    actions: {
+        paddingBottom: 0,
+    },
+    buyIcon: {
+        marginLeft: 'auto !important',
+    },
+    playedOnIndicator: {
+        paddingLeft: 16,
+        paddingTop: 0,
+        paddingBottom: 16,
+    },
 });
 
 export default ({ record }) => {
-  const classes = useStyles();
-  const refresh = useRefresh();
-  // eslint-disable-next-line no-unused-vars
-  const [_, { start }] = useTour();
+    const classes = useStyles();
+    const refresh = useRefresh();
+    // eslint-disable-next-line no-unused-vars
+    const [_, { start }] = useTour();
 
-  const [setPlayed] = useMutation(
-    {
-      type: "update",
-      resource: "tours",
-      payload: { id: record.id, data: { playedOn: new Date() } },
-    },
-    {
-      onSuccess: refresh,
-    }
-  );
+    const [setPlayed] = useMutation(
+        {
+            type: 'update',
+            resource: 'tours',
+            payload: { id: record.id, data: { playedOn: new Date() } },
+        },
+        {
+            onSuccess: refresh,
+        }
+    );
 
-  const handlePlayClicked = () => {
-    start(record.tour);
-    setPlayed();
-  };
+    const handlePlayClicked = () => {
+        start(record.tour);
+        setPlayed();
+    };
 
-  return (
-    <Card
-      className={classnames(classes.root, {
-        [classes.visited]: record.playedOn,
-      })}
-      onClick={handlePlayClicked}
-    >
-      <CardMedia
-        className={classes.media}
-        image={`${process.env.PUBLIC_URL}/${record.image}`}
-        title={record.title}
-      ></CardMedia>
-      <CardContent className={classes.content}>
-        {!record.playedOn && (
-          <NewReleasesIcon
-            color="error"
-            fontSize="large"
-            className={classes.newIcon}
-          />
-        )}
-        <Typography gutterBottom variant="h5" component="h2">
-          {record.title}
-        </Typography>
-        <MarkdownField record={record} source="comment" />
-      </CardContent>
-      <CardActions className={classes.actions}>
-        <Button size="small" startIcon={<PlayIcon />} color="primary">
-          Play
-        </Button>
-        <Button
-          size="small"
-          startIcon={<ShoppingCartIcon />}
-          color="secondary"
-          className={classes.buyIcon}
-          href={record.href}
+    return (
+        <Card
+            className={classnames(classes.root, {
+                [classes.visited]: record.playedOn,
+            })}
+            onClick={handlePlayClicked}
         >
-          Buy
-        </Button>
-      </CardActions>
-      <div className={classes.playedOnIndicator}>
-        <Typography variant="caption" display="block" color="textSecondary">
-          {record.playedOn
-            ? `Last played on ${new Date(record.playedOn).toLocaleString()}`
-            : `Never played before`}
-        </Typography>
-      </div>
-    </Card>
-  );
+            <CardMedia
+                className={classes.media}
+                image={`${process.env.PUBLIC_URL}/${record.image}`}
+                title={record.title}
+            ></CardMedia>
+            <CardContent className={classes.content}>
+                {!record.playedOn && (
+                    <NewReleasesIcon
+                        color="error"
+                        fontSize="large"
+                        className={classes.newIcon}
+                    />
+                )}
+                <Typography gutterBottom variant="h5" component="h2">
+                    {record.title}
+                </Typography>
+                <MarkdownField record={record} source="comment" />
+            </CardContent>
+            <CardActions className={classes.actions}>
+                <Button size="small" startIcon={<PlayIcon />} color="primary">
+                    Play
+                </Button>
+                <Button
+                    size="small"
+                    startIcon={<ShoppingCartIcon />}
+                    color="secondary"
+                    className={classes.buyIcon}
+                    href={record.href}
+                >
+                    Buy
+                </Button>
+            </CardActions>
+            <div className={classes.playedOnIndicator}>
+                <Typography
+                    variant="caption"
+                    display="block"
+                    color="textSecondary"
+                >
+                    {record.playedOn
+                        ? `Last played on ${new Date(
+                              record.playedOn
+                          ).toLocaleString()}`
+                        : `Never played before`}
+                </Typography>
+            </div>
+        </Card>
+    );
 };
