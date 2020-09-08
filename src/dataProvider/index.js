efc1b346eb88232bb0f17fc777ebd117e3577838
JulienM <julienm@marmelab.com>
2020-09-08 11:10:28 +0200

    Add lock methods in ProductEdit

--- src/dataProvider/index.js
+++ src/dataProvider/index.js
@@ -1,5 +1,6 @@
 import simpleRestProvider from 'ra-data-simple-rest';
 import compose from 'recompose/compose';
+import { addLocksMethodsBasedOnALockResource } from '@react-admin/ra-realtime';
 
 import addTreeMethodsBasedOnChildren from './addTreeMethodsBasedOnChildren';
 import addRealtimeMethodsWithFakeTransport from './addRealtimeMethodsWithFakeTransport';
@@ -8,6 +9,7 @@ import localStorageProvider from './localStorageProvider';
 import defaultState from '../tours/data';
 
 const restProvider = compose(
+    addLocksMethodsBasedOnALockResource,
     addRealtimeMethodsWithFakeTransport,
     addTreeMethodsBasedOnChildren
 )(simpleRestProvider('http://localhost:4000'));
