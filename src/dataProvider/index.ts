b19bb3ba62f1eba460a0a66f4164fc5ec27cee8c
Luwangel <adrien.amoros@gmail.com>
2020-10-29 15:48:58 +0100

    Create a custom addSearchMethod data provider

--- src/dataProvider/index.ts
+++ src/dataProvider/index.ts
@@ -2,9 +2,9 @@ import simpleRestProvider from 'ra-data-simple-rest';
 import compose from 'recompose/compose';
 import { addLocksMethodsBasedOnALockResource } from '@react-admin/ra-realtime';
 import { addTreeMethodsBasedOnChildren } from '@react-admin/ra-tree';
-import { addSearchMethod } from '@react-admin/ra-search';
 
 import addRealtimeMethodsWithFakeTransport from './addRealtimeMethodsWithFakeTransport';
+import addSearchMethod from './addSearchMethod';
 import localStorageProvider from './localStorageProvider';
 
 import defaultState from '../tours/data';
