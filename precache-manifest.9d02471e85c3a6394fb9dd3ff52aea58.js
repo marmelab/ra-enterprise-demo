0b4163ee77c811162fe4a80a19c4061bbd64883f
fzaninotto <fzaninotto@gmail.com>
2020-10-31 17:52:54 +0100

    Updates

---
+++ precache-manifest.9d02471e85c3a6394fb9dd3ff52aea58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6ef69246312135738964fc51044161",
+    "url": "/ra-enterprise-demo/index.html"
+  },
+  {
+    "revision": "5bbb79d80cc8ef685502",
+    "url": "/ra-enterprise-demo/static/css/2.7599a9d4.chunk.css"
+  },
+  {
+    "revision": "1d18e6fd22c83cd3ccf3",
+    "url": "/ra-enterprise-demo/static/css/main.606778b9.chunk.css"
+  },
+  {
+    "revision": "5bbb79d80cc8ef685502",
+    "url": "/ra-enterprise-demo/static/js/2.3f69f0fa.chunk.js"
+  },
+  {
+    "revision": "979fef6175eee4710a9afcf2ec8a2ff8",
+    "url": "/ra-enterprise-demo/static/js/2.3f69f0fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d18e6fd22c83cd3ccf3",
+    "url": "/ra-enterprise-demo/static/js/main.f2750a01.chunk.js"
+  },
+  {
+    "revision": "8a2a5ad229bf69e77332",
+    "url": "/ra-enterprise-demo/static/js/runtime-main.5d6bb614.js"
+  },
+  {
+    "revision": "87ada4beb03d166bfce221261bda50f2",
+    "url": "/ra-enterprise-demo/static/media/welcome_illustration.87ada4be.svg"
+  }
+]);
