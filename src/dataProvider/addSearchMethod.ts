cdf58abe0ad91a78449c63a7844d39f64990fc76
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2020-11-12 09:52:11 +0100

    Reduce network payload

--- src/dataProvider/addSearchMethod.ts
+++ src/dataProvider/addSearchMethod.ts
@@ -31,16 +31,36 @@ const addSearchMethod = dataProvider => {
 
 export default addSearchMethod;
 
+const searchInResource = async (dataProvider, resource, query) => {
+    const { data, total } = await dataProvider.getList(resource, {
+        filter: { q: query },
+        pagination: { page: 1, perPage: 5 },
+        sort: { field: 'id', order: 'ASC' },
+    });
+
+    return {
+        data: await Promise.all(
+            data.map(async record => ({
+                id: `${resource}/${record.id}`,
+                type: resource,
+                url: `/${resource}/${record.id}`,
+                content: await ResultBuilders[resource](record, dataProvider),
+            }))
+        ),
+        total,
+    };
+};
+
 const ResultBuilders = {
     customers: async (record, dataProvider) => {
         const orders = await dataProvider.getList('commands', {
             filter: { customer_id: record.id, status: 'ordered' },
-            pagination: { page: 1, perPage: 100 },
+            pagination: { page: 1, perPage: 1 },
             sort: { field: 'id', order: 'ASC' },
         });
         const reviews = await dataProvider.getList('reviews', {
             filter: { customer_id: record.id },
-            pagination: { page: 1, perPage: 100 },
+            pagination: { page: 1, perPage: 1 },
             sort: { field: 'id', order: 'ASC' },
         });
 
@@ -53,7 +73,7 @@ const ResultBuilders = {
     products: async (record, dataProvider) => {
         const reviews = await dataProvider.getList('reviews', {
             filter: { product_id: record.id },
-            pagination: { page: 1, perPage: 100 },
+            pagination: { page: 1, perPage: 1 },
             sort: { field: 'id', order: 'ASC' },
         });
 
@@ -75,24 +95,4 @@ const ResultBuilders = {
     reviews: async record => {
         return record;
     },
-};
-
-const searchInResource = async (dataProvider, resource, query) => {
-    const { data, total } = await dataProvider.getList(resource, {
-        filter: { q: query },
-        pagination: { page: 1, perPage: 10 },
-        sort: { field: 'id', order: 'ASC' },
-    });
-
-    return {
-        data: await Promise.all(
-            data.map(async record => ({
-                id: `${resource}/${record.id}`,
-                type: resource,
-                url: `/${resource}/${record.id}`,
-                content: await ResultBuilders[resource](record, dataProvider),
-            }))
-        ),
-        total,
-    };
 };
