8fcf8ae797e99a3c3c332f9bcedb1ef9aae3a07d
djhi <djhi@users.noreply.github.com>
2025-02-10 11:03:47 +0000

    deploy: 8ade5df715c14d74f3b0524b043e36464a37a380

--- assets/TourLauncher-BoS9HveQ.js
+++
@@ -1,2 +0,0 @@
-var u=Object.defineProperty;var a=(r,s)=>u(r,"name",{value:s,configurable:!0});import{m as e,b as o,r as n,j as c,N as f}from"./index-BPq_hhta.js";const x=a(()=>{const{tour:r}=e(),[{running:s},{start:t}]=o();return n.useEffect(()=>{if(t&&!s&&r){t(r);return}},[s,t,r]),s?c.jsx(f,{to:"/"}):null},"TourLauncher");export{x as default};
-//# sourceMappingURL=TourLauncher-BoS9HveQ.js.map
