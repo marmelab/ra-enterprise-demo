b9de74945174feaec3b017c00c4391b94e51b34d
djhi <djhi@users.noreply.github.com>
2025-03-13 09:28:33 +0000

    deploy: 89e807dd5bbf1f289a1f31a71ffc7957071f4ae7

---
+++ assets/TourLauncher-vFtrk0A4.js
@@ -0,0 +1,2 @@
+var u=Object.defineProperty;var a=(r,s)=>u(r,"name",{value:s,configurable:!0});import{m as e,b as o,r as n,j as c,N as f}from"./index-aKJeAhUa.js";const x=a(()=>{const{tour:r}=e(),[{running:s},{start:t}]=o();return n.useEffect(()=>{if(t&&!s&&r){t(r);return}},[s,t,r]),s?c.jsx(f,{to:"/"}):null},"TourLauncher");export{x as default};
+//# sourceMappingURL=TourLauncher-vFtrk0A4.js.map
