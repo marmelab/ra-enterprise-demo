e575f6e135efc11b27a4cf28e81be4af34f59f8b
djhi <djhi@users.noreply.github.com>
2026-01-14 14:46:22 +0000

    deploy: d53ee7b3247bfbb2b71fa83e67d1b3cb4459b5fa

--- assets/TourLauncher-D3WjGYVH.js
+++
@@ -1,2 +0,0 @@
-var u=Object.defineProperty;var a=(r,s)=>u(r,"name",{value:s,configurable:!0});import{m as e,b as n,r as o,j as c,n as f}from"./index-C9lNGrfq.js";const x=a(()=>{const{tour:r}=e(),[{running:s},{start:t}]=n();return o.useEffect(()=>{if(t&&!s&&r){t(r);return}},[s,t,r]),s?c.jsx(f,{to:"/"}):null},"TourLauncher");export{x as default};
-//# sourceMappingURL=TourLauncher-D3WjGYVH.js.map
